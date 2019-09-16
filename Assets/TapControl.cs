3e8494d4289023d22715e921037a6cba92f047a8
相本廉 <ren.aimoto3.6@gmail.com>
2019-09-17 05:07:38 +0900

    PinBallプロジェクト　初めてのコミット
    Unityで簡単なピンボールのゲームを作成しました、発展課題まで終了済み。

---
+++ Assets/TapControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapControl : MonoBehaviour {
+	private 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
